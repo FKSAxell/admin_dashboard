8a940ba8758e3194ccf58f0eb026f8a6f4aeb35e
aconcha <kevin.concha.regatto@outlook.com>
2022-02-09 23:47:58 -0500

    dashboard y blank view

--- lib/ui/layouts/dashboard/dashboard_layout.dart
+++ lib/ui/layouts/dashboard/dashboard_layout.dart
@@ -37,7 +37,13 @@ class _DashboardLayoutState extends State<DashboardLayout>
                 child: Column(
                   children: [
                     NavBar(),
-                    Expanded(child: widget.child),
+                    Expanded(
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        child: widget.child,
+                      ),
+                    ),
                   ],
                 ),
               ),
